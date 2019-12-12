7f1fc73862c4ec8118805d50b6e45abd5e3fd95f
alexey2baranov <alexey2baranov@gmail.com>
2019-12-12 07:54:04 +0300

    fix doc

---
+++ public/auth-test.js
@@ -0,0 +1,31 @@
+const transport = axios.create({
+    baseURL: 'https://dev.kopnik.org/auth-test/',
+    withCredentials: true,
+    credentials: 'include',
+    headers: {
+        // 'X-Custom-Header': 'foobar',
+        'Content-Type': 'plain/text'
+    }
+});
+
+function login_click() {
+    fetch('https://dev.kopnik.org/auth-test/login?val=1234',{
+        method: 'POST',
+        credentials: 'include',
+        headers:{
+            'Content-Type': 'text/plain',
+        }
+    })
+}
+
+function anyMethod_click() {
+/*    fetch('https://dev.kopnik.org/auth-test/any-method',{
+        method: 'POST',
+        credentials: 'include',
+        headers:{
+            'Content-Type': 'text/plain',
+        }
+    })*/
+
+    transport.post('any-method',null)
+}
