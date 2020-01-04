8c1953c7a9e7f28be5f5350c2cd548553960ced8
Alexey Kaytaz <alexey@kaytaz.ru>
2020-01-04 17:25:54 +0100

    fixes for publish.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export WEBPORT_CLIENT="${WEBPORT_CLIENT:-latest}"
+export WEBPORT_CLIENT="${WEBPORT_CLIENT:-80}"
 docker-compose build
--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 
 export DOCKER_TAG="${DOCKER_TAG:-latest}"
 echo $DOCKER_TAG
-docker tag  kopnik-client_npm_build  kopnik/kopnik-client:${DOCKER_TAG}
+docker tag kopnik-client_nginx_build:latest kopnik/kopnik-client:${DOCKER_TAG}
 docker push kopnik/kopnik-client:latest
