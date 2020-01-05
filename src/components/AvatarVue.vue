13d3519610c975faf61c0761315f1884f23e56a8
alexey2baranov <alexey2baranov@gmail.com>
2020-01-05 22:31:29 +0300

    +AvatarVue

---
+++ src/components/AvatarVue.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-badge :content="value.rank" bottom :offset-x="(size/64)*7+14" :offset-y="(size/64)*7+14"
+             color="orange">
+        <v-avatar :size="size"
+                  @click="$emit('click', $event)" @dblclick="$emit('dblclick', $event)">
+            <img :src="value.photo" style="object-fit: cover; "/>
+        </v-avatar>
+    </v-badge>
+</template>
+<script>
+    import logger from "./mixin/logger"
+    import {Kopnik} from "../models";
+
+    export default {
+        name: "Avatar",
+        mixins: [logger],
+        components: {
+        },
+        data: () => {
+            return {
+            }
+        },
+        props: {
+            size: {
+                type: Number,
+                default: 48
+            },
+            value: {
+                type: Kopnik
+            },
+        },
+        async created() {
+            await this.value.loaded()
+        },
+        async mounted() {
+        }
+    }
+</script>
