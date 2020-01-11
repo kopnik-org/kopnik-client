fc85e1b8b7f799106a1ffc75432f87dfab17d9c6
alexey2baranov <alexey2baranov@gmail.com>
2020-01-11 20:50:13 +0300

    +Ten test

---
+++ tests/unit/components/Ten.vue.spec.js
@@ -0,0 +1,34 @@
+import flushPromises from "flush-promises";
+import Vue from 'vue'
+
+import vuePlugins from "../../test-setup";
+import KTen from "../../../src/components/KTen";
+import {bottle, container} from "../../../src/plugins/bottle";
+import {Kopnik} from "../../../src/models";
+
+describe('unit components Ten', () => {
+    beforeAll(async () => {
+        await login(6)
+    })
+
+    beforeEach(() => {
+    })
+
+    it('draw', async () => {
+        const kopnik= Kopnik.getReference(6)
+        await container.application.authenticate()
+        kopnik.ten= [7,].map(eachDruzheId=>Kopnik.getReference(eachDruzheId))
+        kopnik.witnessRequests= [1,].map(eachDruzheId=>Kopnik.getReference(eachDruzheId))
+        const vm = new Vue({
+            ...KTen,
+            ...vuePlugins,
+            propsData:{
+                value: kopnik
+            }
+        })
+        vm.$mount()
+        await flushPromises()
+        expect(vm.$el).toMatchSnapshot()
+    })
+})
+
