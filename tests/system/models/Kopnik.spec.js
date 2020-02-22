e2e639dd82d26425e53b16f4d16f0e84b494fd47
alexey2baranov <alexey2baranov@gmail.com>
2020-02-22 15:01:56 +0300

    +Kopnik test

--- tests/system/models/Kopnik.spec.js
+++ tests/system/models/Kopnik.spec.js
@@ -12,4 +12,39 @@ describe('system models Kopnik', () => {
         AbstractSync.clearCache()
     })
 
+    it('locale present', async () => {
+        await login(1)
+        const kopnik=  await Kopnik.get(1)
+        expect(kopnik.locale).toBe('ru')
+    })
+    it('location is object', async () => {
+        await login(1)
+        const kopnik=  await Kopnik.get(1)
+        expect(kopnik.location).toHaveProperty('lat')
+        expect(kopnik.location).toHaveProperty('lng')
+    })
+    it('updateLocale', async () => {
+        await login(1)
+        const kopnik=  await Kopnik.get(1)
+        await kopnik.updateLocale('ru')
+    })
+    it('isMessagesFromGroupAllowed', async () => {
+        await login(1)
+        const kopnik=  await Kopnik.get(1)
+        const result= await kopnik.isMessagesFromGroupAllowed()
+        expect(result).toBe(true)
+    })
+    it('getWitnessRequests have passport', async () => {
+        await login(1)
+        const kopnik= Kopnik.getReference(1)
+        await kopnik.reloadWitnessRequests()
+        expect(kopnik.witnessRequests).toBeInstanceOf(Array)
+        expect(kopnik.witnessRequests[0]).toHaveProperty('passport')
+        expect(kopnik.witnessRequests[0].passport).toBe(5220)
+    })
+    it('get havn\'t passport', async () => {
+        await login(1)
+        const kopnik=  await Kopnik.get(3)
+        expect(kopnik.passport).toBeFalsy()
+    })
 })
