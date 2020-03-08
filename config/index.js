cdd5a699791bf0c79ae854f0e6da1bf7493fe2d7
alexey2baranov <Ir1n@gh>
2020-03-09 00:33:36 +0300

    some

--- config/index.js
+++ config/index.js
@@ -8,17 +8,13 @@ let config = {
             // clientId: 7210289,
             // страница, на которую нужно перейти для аутентификации
             // loginUrl:'https://dev.kopnik.org/connect/vkontakte',
-            loginUrl: 'http://localhost:8080/connect/vkontakte',
-            // loginUrl:'http://192.168.43.9:8082/connect/vkontakte',
-            // страница не которую ВК сделает редирект после аутентификации и передаст данные
-            // redirectUrl: 'https://dev.kopnik.org/login/check-vk'
-            // redirectUrl: 'http://localhost:8082/login/check-vk'
-            // redirectUrl: 'http://192.168.43.9:8082/login/check-vk'
+            // loginUrl: 'http://localhost:8081/connect/vkontakte',
+            loginUrl: 'http://localhost:8082/connect/vkontakte'
         },
         api: {
             // path: "https://dev.kopnik.org/api"
+            // path: "http://localhost:8081/api"
             path: "http://localhost:8082/api"
-            // path: "http://192.168.43.9:8082/api"
         },
         di: {
             fetch: true,
