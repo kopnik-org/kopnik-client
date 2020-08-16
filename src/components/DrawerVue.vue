a2eb42466b7b702a8a5a850904d9cb776df0d949
alexey2baranov <Ir1n@gh>
2020-08-16 03:28:14 +0300

    fix snow < 60 lat

--- src/components/DrawerVue.vue
+++ src/components/DrawerVue.vue
@@ -1,166 +1,167 @@
 <template>
-    <v-navigation-drawer :value="value" temporary touchless
-                         @input="drawer_input" app>
-        <router-link to="/profile" class="cursor-pointer" tag="div">
-            <kopnik-vue v-if="application.user" :value="application.user" to="/profile"
-                        :avatar-size="64" class="flex-grow-1">
-            </kopnik-vue>
-        </router-link>
-        <v-divider></v-divider>
-        <v-list>
+  <v-navigation-drawer :value="value" temporary touchless
+                       @input="drawer_input" app>
+    <router-link to="/profile" class="cursor-pointer" tag="div">
+      <kopnik-vue v-if="application.user" :value="application.user" to="/profile"
+                  :avatar-size="64" class="flex-grow-1">
+      </kopnik-vue>
+    </router-link>
+    <v-divider></v-divider>
+    <v-list>
 
-            <v-list-item link to="/">
-                <v-list-item-action>
-                    <v-icon>mdi-home-city</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{ $t('drawer.map') }}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-list-item subordinates :disabled="!application.user" @click="subordinates_click">
-                <v-list-item-action>
-                    <v-icon>mdi-account-multiple</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{ $t('drawer.myTen') }}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-list-item link :disabled="!application.user"
-                         @click="throwUnderConstructionError"
-            >
-                <v-list-item-action>
-                    <v-icon>mdi-chat</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{ $t('drawer.tenChat') }}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-list-item link
-                         @click="throwUnderConstructionError"
-                         :disabled="!application.user"
-            >
-                <v-list-item-action>
-                    <v-icon>mdi-chat</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{ $t('drawer.foremanChat') }}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-list-item link to="/Witness"
-                         v-if="application.user.isWitness"
-                         :disabled="!application.user">
-                <v-list-item-action>
-                    <v-icon>mdi-human-greeting</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{ $t('drawer.witnessRequests') }}</v-list-item-title>
-                </v-list-item-content>
-                <v-list-item-action>
-                    <v-icon>mdi-account-question</v-icon>
-                </v-list-item-action>
-            </v-list-item>
-            <v-list-item link to="/thanks">
-                <v-list-item-action>
-                    <v-icon>mdi-heart</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{ $t('drawer.thanks') }}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-list-item link>
-                <v-list-item-action>
-                    <v-icon>mdi-help-circle</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{ $t('drawer.help') }}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-divider></v-divider>
-            <v-list-item logout link @click="logout_click">
-                <v-list-item-action>
-                    <v-icon>mdi-location-exit</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{ $t('drawer.logout') }}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-            <v-list-item v-if="application.user || application.user.id===1" @click="snow_click">
-                <v-list-item-action>
-                    <v-icon>mdi-location-snow</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title> Снежинка</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-        </v-list>
-    </v-navigation-drawer>
+      <v-list-item link to="/">
+        <v-list-item-action>
+          <v-icon>mdi-home-city</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ $t('drawer.map') }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item subordinates :disabled="!application.user" @click="subordinates_click">
+        <v-list-item-action>
+          <v-icon>mdi-account-multiple</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ $t('drawer.myTen') }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link :disabled="!application.user"
+                   @click="throwUnderConstructionError"
+      >
+        <v-list-item-action>
+          <v-icon>mdi-chat</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ $t('drawer.tenChat') }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link
+                   @click="throwUnderConstructionError"
+                   :disabled="!application.user"
+      >
+        <v-list-item-action>
+          <v-icon>mdi-chat</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ $t('drawer.foremanChat') }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link to="/Witness"
+                   v-if="application.user.isWitness"
+                   :disabled="!application.user">
+        <v-list-item-action>
+          <v-icon>mdi-human-greeting</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ $t('drawer.witnessRequests') }}</v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-icon>mdi-account-question</v-icon>
+        </v-list-item-action>
+      </v-list-item>
+      <v-list-item link to="/thanks">
+        <v-list-item-action>
+          <v-icon>mdi-heart</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ $t('drawer.thanks') }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-action>
+          <v-icon>mdi-help-circle</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ $t('drawer.help') }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list-item logout link @click="logout_click">
+        <v-list-item-action>
+          <v-icon>mdi-location-exit</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ $t('drawer.logout') }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item v-if="application.user || application.user.id===1" @click="snow_click">
+        <v-list-item-action>
+          <v-icon>mdi-location-snow</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title> Снежинка</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
 
-    import KopnikVue from "./KopnikVue";
-    import {container} from "../bottle/bottle";
-    import logger from "./mixin/logger";
-    import Kopnik from "../models/Kopnik";
-    import createSubordinates from "../utils/snow/createSubordinates";
+import KopnikVue from "./KopnikVue";
+import {container} from "../bottle/bottle";
+import logger from "./mixin/logger";
+import Kopnik from "../models/Kopnik";
+import createSubordinates from "../utils/snow/createSubordinates";
 
-    export default {
-        mixins: [logger],
-        components: {
-            KopnikVue,
-        },
-        data() {
-            return {
-                application: container.application,
-                KopnikStatus: Kopnik.Status,
-            }
-        },
-        props: {
-            value: {
-                type: Boolean
-            }
-        },
-        methods: {
-            async subordinates_click() {
-                if (await this.application.forwardUserToBeConfirmed()){
-                    return
-                }
-                await this.application.lockSection(async ()=>{
-                    await this.application.setSection(Application.Section.Ten)
-                })
-            },
-            throwUnderConstructionError() {
-                throw new Error(this.$t(this.application.getMessage('errors.underConstruction')))
-            },
-            drawer_input(event) {
-                // console.log('event', event)
-                this.$emit('input', event)
-            },
-            async logout_click() {
-                await this.application.logout()
-            },
-            async snow_click() {
-                const tsar = await Kopnik.create({
-                    firstName: 'Гора',
-                    lastName: 'Мира',
-                    patronymic: 'Рука',
-                    location: {
-                        lat: 58.1996,
-                        lng: 68.256,
-                    },
-                    // identifier: 261824271
-                })
-                await createSubordinates(tsar, 30)
-            }
-        },
-        async created() {
-            await this.application.resolveUser()
-        }
+export default {
+  mixins: [logger],
+  components: {
+    KopnikVue,
+  },
+  data() {
+    return {
+      application: container.application,
+      KopnikStatus: Kopnik.Status,
     }
+  },
+  props: {
+    value: {
+      type: Boolean
+    }
+  },
+  methods: {
+    async subordinates_click() {
+      if (await this.application.forwardUserToBeConfirmed()) {
+        return
+      }
+      await this.application.lockSection(async () => {
+        await this.application.setSection(Application.Section.Ten)
+      })
+    },
+    throwUnderConstructionError() {
+      throw new Error(this.$t(this.application.getMessage('errors.underConstruction')))
+    },
+    drawer_input(event) {
+      // console.log('event', event)
+      this.$emit('input', event)
+    },
+    async logout_click() {
+      await this.application.logout()
+    },
+    async snow_click() {
+      /*                const tsar = await Kopnik.create({
+                          firstName: 'Гора',
+                          lastName: 'Мира',
+                          patronymic: 'Рука',
+                          location: {
+                              lat: 58.1996,
+                              lng: 68.256,
+                          },
+                          // identifier: 261824271
+                      })*/
+      const tsar = await Kopnik.get(1)
+      await createSubordinates(tsar, 30)
+    }
+  },
+  async created() {
+    await this.application.resolveUser()
+  }
+}
 </script>
 
 <style scoped>
-    .cursor-pointer {
-        cursor: pointer;
-    }
+.cursor-pointer {
+  cursor: pointer;
+}
 </style>
--- src/utils/snow/createSubordinates.js
+++ src/utils/snow/createSubordinates.js
@@ -11,26 +11,29 @@ const MIN_DISTANCE = 0.33
 const K_DISTANCE = 0.33
 
 export default async function createSubordinates(foreman, distance) {
-    const subordinatesCount = 4 + _.random(0, 5)
-    let angle = 0
-    for (let i = 0; i < subordinatesCount; i++) {
-        angle += 2 * Math.PI / subordinatesCount
-        const eachSubordinateLocation = getPointInDirection(foreman.location, randomize(distance), randomize(angle))
-        const eachSubordinateNextDistance = randomize(distance * K_DISTANCE)
-        const isEachSubordinateForeman = eachSubordinateNextDistance >= MIN_DISTANCE
-        const eachSubordinate = await Kopnik.create({
-            firstName: dic.name[_.random(0, dic.name.length)],
-            lastName: dic.lastName[_.random(0, dic.lastName.length)],
-            patronymic: dic.middleNameMale[_.random(0, dic.middleNameMale.length)],
-            location: {
-                lat: eachSubordinateLocation.lat,
-                lng: eachSubordinateLocation.lng,
-            },
-            foreman_id: foreman.id,
-            photo: isEachSubordinateForeman ? avatars[_.random(0, avatars.length)] : avatars2[_.random(0, avatars2.length)]
-        })
-        if (isEachSubordinateForeman) {
-            await createSubordinates(eachSubordinate, eachSubordinateNextDistance)
-        }
+  const subordinatesCount = 4 + _.random(0, 5)
+  let angle = 0
+  for (let i = 0; i < subordinatesCount; i++) {
+    angle += 2 * Math.PI / subordinatesCount
+    const eachSubordinateLocation = getPointInDirection(foreman.location, randomize(distance), randomize(angle))
+    if (eachSubordinateLocation.lat > 70) {
+      continue
     }
+    const eachSubordinateNextDistance = randomize(distance * K_DISTANCE)
+    const isEachSubordinateForeman = eachSubordinateNextDistance >= MIN_DISTANCE
+    const eachSubordinate = await Kopnik.create({
+      firstName: dic.name[_.random(0, dic.name.length)],
+      lastName: dic.lastName[_.random(0, dic.lastName.length)],
+      patronymic: dic.middleNameMale[_.random(0, dic.middleNameMale.length)],
+      location: {
+        lat: eachSubordinateLocation.lat,
+        lng: eachSubordinateLocation.lng,
+      },
+      foreman_id: foreman.id,
+      photo: isEachSubordinateForeman ? avatars[_.random(0, avatars.length)] : avatars2[_.random(0, avatars2.length)]
+    })
+    if (isEachSubordinateForeman) {
+      await createSubordinates(eachSubordinate, eachSubordinateNextDistance)
+    }
+  }
 }
