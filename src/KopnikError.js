923b4401cad6bc3ed354ef4d5bb7709cd07c63b8
alexey2baranov <Ir1n@gh>
2020-09-07 08:51:05 +0300

    imr errors

--- src/KopnikError.js
+++ src/KopnikError.js
@@ -1,20 +1,22 @@
 export class KopnikError extends Error {
-    /**
-     * @param  {string} message
-     * @param  {Error?} base
-     * @param  {Number} code
-     * 1-Under construction
-     * 2 No message provided (in locale messages)
-     */
-    constructor(message, code, base) {
-        super(message)
-        this.code = code
-        this.base= base
-    }
+  /**
+   * @param  {string} message
+   * @param  {Error?} base
+   * @param  {Number} code
+   * 1-Under construction
+   * 2 No message provided (in locale messages)
+   */
+  constructor(message, code, base) {
+    super(message)
+    this.code = code
+    this.base = base
+  }
 }
+
 export class KopnikApiError extends KopnikError {
-    constructor(message, code, url, base) {
-        super(message, code, base)
-        this.url = url
-    }
+  constructor(message, code, url, trace, base) {
+    super(message, code, base)
+    this.url = url
+    this.trace = trace
+  }
 }
--- tests/toBeKopnikError.js
+++ tests/toBeKopnikError.js
@@ -2,7 +2,7 @@
 import {KopnikError} from "../src/KopnikError";
 
 export default function toBeKopnikError(error, match) {
-    const {code, message}= (typeof match === 'number')?{code:match}:(typeof match === 'string')?{message: match}:match
+    const {code, message}= (typeof match === 'number')?{code:match}:(typeof match === 'string')?{message: match}:(match||{})
     const pass = (error instanceof KopnikError) && (!code || code===error.code) && (!message || error.message.match(message))
     if (pass) {
         return {
