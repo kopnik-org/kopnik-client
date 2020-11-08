292fad2977ae4923c72c08adb7ba16630d5f521e
alexey2baranov <Ir1n@gh>
2020-11-08 22:50:56 +0300

    + pl locale

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,11 +1,13 @@
-import ru from './ru'
-import en from './en'
+import ru from './ru.json'
+import en from './en.json'
 import sk from './sk.json'
+import pl from './pl.json'
 
 const messages = {
   ru,
   en,
   sk,
+  pl,
 }
 
 export default messages
