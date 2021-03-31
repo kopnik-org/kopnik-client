130607d6102e1ad98f4c214b4c7fd3feb16bb227
alexey2baranov <Ir1n@gh>
2021-03-31 21:38:48 +0300

    feat (i18): +de empty

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,13 +1,15 @@
-import ru from './ru.json'
-import en from './en.json'
-import sk from './sk.json'
-import pl from './pl.json'
+import ru from './ru/main.json'
+import en from './en/main.json'
+import sk from './sk/main.json'
+import pl from './pl/main.json'
+import de from './de/main.json'
 
 const messages = {
   ru,
   en,
   sk,
   pl,
+  de,
 }
 
 export default messages
